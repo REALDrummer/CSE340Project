f31e14eac9a92c170db980516ab91a7ca77ec167
REALDrummer <conman.davey@gmail.com>
2015-02-09 01:16:32 -0700

    I made a Queue struct and added functions and doc.
    I thought it would be handy to have a Queue struct to keep track of whole queues, especially their head and their tail since we'l be accessing both a lot for enqueuing and dequeuing, respectively. We could in theory just keep track of one or the other, but then we would have to iterate through the whole queue each time we needed the one we didn't track, so I figured it was more efficient this way.
    
    I was unsure of whether or not it would be helpful to keep track of the size of the Queues, so I left that as a commented line in the Queue struct declaration.
    
    I also changed the payload to a `void*` so that we can put anything we want in the queue. 
    
    Finally, I added or renamed and documented all our functions. However, I was unsure of your intentions with `rotateQ`; what should that do and is there a reason it was named with a "Q" instead of "Queue" like the others?

--- q.h
+++ q.h
@@ -1,23 +1,56 @@
 //CSE430 Project 1
-//Zach Josephson and Connor Davey
+//@author Zach Josephson and Connor Davey
+
+struct Queue {
+	struct Element* head = NULL;
+	struct Element* tail = NULL;
+	// int size = 0;
+}
 
 struct Element {
-	int payload;
+	void* payload;
 	struct Element* next;
 	struct Element* previous;
 };
 
 //GLOBAL VARIABLES
 
-
 //FORWARD DECLATATIONS
-struct Element* NewItem();
-struct Element* InitQueue(struct Element* head);
-void AddQueue(struct Element* head, struct Element* item);
-struct Element* DelQueue(struct Element* head);
-void RotateQ(struct Element* head);
+/** This function adds a new element to the end of the Queue.
+ * 
+ * @param payload 
+ * 		is a pointer to the data to put inside the new Queue Element.
+ * @return a pointer to the Element created to contain the given payload.*/
+struct Element* enqueue(void* payload);
 
+/** This function removes the Element at the beginning the given Queue.
+ * 
+ * @param queue
+ * 		is a pointer to the Queue that will have its first element dequeued.
+ * 
+ * @return a pointer to the payload of the Element that was removeed from the Queue. */
+void* dequeue(struct Queue* queue);
 
+/** This function "peeks" at the last Element in the Queue.
+ * 
+ * @param queue
+ * 		is a pointer to the queue which we want the payload of the last Element from.
+ * 
+ * @return a pointer to the payload of the last Element in the Queue */
+void* peek(struct Queue* queue);
 
+/** This function creates and initializes a new Queue.
+ * 
+ * @return a pointer to the new Queue struct.*/
+struct Queue* newQueue();
 
+/** This function deletes the given Queue.
+ * 
+ * @param queue
+ * 		is a pointer to the Queue to be deleted.
+ * 
+ * @return the number of Elements that were present in the deleted Queue. */
+int deleteQueue(struct Queue* queue);
 
+/** TODO: What should this do? */
+void rotateQ(struct Queue* queue);
