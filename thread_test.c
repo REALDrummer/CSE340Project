ce4739f84f53f2b03a13982d53d8e3dadc885f07
Zach Josephson <zjosephson@gmail.com>
2015-03-19 13:24:02 -0700

    converted functions from main.c to thread_test.c and included infinite loops, but need yield

--- thread_test.c
+++ thread_test.c
@@ -3,6 +3,39 @@
 //Global variables
 struct Queue* run_queue;
 
-null main (){
-	printf("Hello World");
+int x = 0;
+
+void counter(){
+	do {
+		x++;
+	} while (true);
+} 
+
+void printEven(){
+	do {
+		if ((x % 2) == 0){
+			printf("%d", x);
+		}
+	} while (true);
+}
+
+void printOdd(){
+	do {
+		if ((x % 2) == 1){
+			printf("%d", x);
+		}
+	} while (true);
+}
+
+void main (){
+	//initialize the run queue
+	run_queue = newQueue();
+
+	//start threads
+	start_thread(counter);
+	start_thread(printEven);
+	start_thread(printOdd);
+
+	run();
+	
 }
