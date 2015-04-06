b1d00a0cb4df2ae8acd0e7d662ef55cd2e7abbfd
Zach Josephson <zjosephson@gmail.com>
2015-04-06 13:58:38 -0700

    attempted to define infite loop functions in proj-3.c

--- proj-3.c
+++ proj-3.c
@@ -2,5 +2,27 @@
 //@author Zach Josephson and Connor Davey
 
 
- #include threads.h
+ #include <threads.h>
+
+//Global variables
+int x = 0;
+
+struct Semaphore* run_sem;
+
+void counter(){
+	do{
+		P(run_sem);
+		x++;
+		V(run_sem);
+	} while(true)
+}
+
+void printCounter(){
+	do {
+		P(run_sem);
+		printf("%d", x);
+		V(run_sem);
+	} while(true)
+}
+
 
