94c8ef41aa0b2dde5806307d31966bc674527ddd
REALDrummer <conman.davey@gmail.com>
2015-04-21 16:22:54 -0700

    I added an implementation, but it's not done yet.
    There's a Segmentation fault happening, but it does read and write
    successfully! We may have to find some way to confirm that readers are
    actually able to read at the same time as each other, though.
    
    Signed-off-by: REALDrummer <conman.davey@gmail.com>

--- proj-4.c
+++ proj-4.c
@@ -2,10 +2,138 @@
 //@author Zach Josephson and Connor Davey
 
 #include "sem.h"
+#include <stdio.h>
+
+#define true 1
+
+struct Semaphore *reading, *writing;
+
+int next_number = 0;
 
 //Global Variables
+void reader1() {
+	while (true) {
+		reading->value++;
+		
+		FILE* file = fopen("test file.txt", "r");
+		if (file == NULL) {
+			printf("Reader 1 couldn't open the file!\n");
+			exit(2);
+		}
+		
+		unsigned int read_number;
+		while (fscanf(file, "%d\n", &read_number) != EOF)
+			printf("R1 read %d\n", read_number);
+			
+		fclose(file);
+		
+		reading->value--;
+		
+		yield();
+	}
+}
+
+void reader2() {
+	while (true) {
+		reading->value++;
+		
+		FILE* file = fopen("test file.txt", "r");
+		if (file == NULL) {
+			printf("Reader 2 couldn't open the file!\n");
+			exit(2);
+		}
+		
+		unsigned int read_number;
+		while (fscanf(file, "%d\n", &read_number) != EOF)
+			printf("R2 read %d\n", read_number);
+			
+		fclose(file);
+		
+		reading->value--;
+		
+		yield();
+	}
+}
+
+void reader3() {
+	while (true) {
+		reading->value++;
+		
+		FILE* file = fopen("test file.txt", "r");
+		if (file == NULL) {
+			printf("Reader 3 couldn't open the file!\n");
+			exit(2);
+		}
+		
+		unsigned int read_number;
+		while (fscanf(file, "%d\n", &read_number) != EOF)
+			printf("R3 read %d\n", read_number);
+			
+		fclose(file);
+		
+		reading->value--;
+		
+		yield();
+	}
+}
+
+void writer1() {
+	while (true) {
+		P(reading);
+		P(writing);
+		
+		FILE* file = fopen("test file.txt", "a");
+		if (file == NULL) {
+			printf("Writer 1 couldn't open the file!\n");
+			exit(2);
+		}
+		
+		fprintf(file, "%d\n", ++next_number);
+		printf("W1 wrote %d\n", next_number);
+		
+		fclose(file);
+		
+		V(writing);
+		
+		yield();
+	}
+}
+
+void writer2() {
+	while (true) {
+		P(reading);
+		P(writing);
+		
+		FILE* file = fopen("test file.txt", "a");
+		if (file == NULL) {
+			printf("Writer 2 couldn't open the file!\n");
+			exit(2);
+		}
+		
+		fprintf(file, "%d\n", ++next_number);
+		printf("W2 wrote %d\n", next_number);
+		
+		fclose(file);
+		
+		V(writing);
+		
+		yield();
+	}
+}
+
 
 int main(){
-
+	reading = newSemaphore(1);
+	writing = newSemaphore(0);
 	
+	run_queue = newQueue();
+	
+	start_thread(reader1);
+	start_thread(reader2);
+	start_thread(reader3);
+	
+	start_thread(writer1);
+	start_thread(writer2);
+	
+	run();
 }
